59fcff3edb10fcc00895c1c501fc89902a9460aa
jiangxuejiao <xujiao.jiang@icloud.com>
2022-02-23 23:04:57 +0800

    准备开发web适配器

---
+++ FrameworkStruct/Packages/Adapter/WebAdapter.swift
@@ -0,0 +1,16 @@
+//
+//  WebAdapter.swift
+//  FrameworkStruct
+//
+//  Created by 蒋雪姣 on 2022/2/23.
+//
+
+/**
+ * Web适配器，主要和web页面对接和交互
+ */
+import UIKit
+
+class WebAdapter: OriginAdapter
+{
+    
+}
