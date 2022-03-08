83068fcb34375b7365cd640e7b0f058a42705052
 蒋 雪姣 <jiangxuejiao@jinfeng.cn>
2022-03-08 17:36:48 +0800

    完善文档

--- FrameworkStruct/BasicObject/AppDelegate.swift
+++ FrameworkStruct/BasicObject/AppDelegate.swift
@@ -37,8 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         self.initData()
-        let w = RegisterLoginWorker()
-        w.abc()
+        
         return true
     }
 
