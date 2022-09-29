7520fe9719bccc24c3b9c14356ecbe316dcf1724
 蒋 雪姣 <jiangxuejiao@jinfeng.cn>
2022-09-29 15:45:58 +0800

    常用工具优化

--- FrameworkStruct/BasicObject/AppDelegate.swift
+++ FrameworkStruct/BasicObject/AppDelegate.swift
@@ -33,7 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate
         //初始化程序组件
         rootMonitor.originConfig()
     }
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         self.initData()
--- FrameworkStruct/Tools/Utility.swift
+++ FrameworkStruct/Tools/Utility.swift
@@ -13,6 +13,7 @@
  */
 import UIKit
 
+//MARK: 打印日志
 ///打印一些信息，包含系统时间,打印的文件信息,要打印的内容
 func printMsg<T>(message: T, fileName: String = #file, methodName: String = #function, lineNumber: Int = #line)
 {
@@ -28,6 +29,25 @@ func FSLog(_ message: String)
     #if DEBUG
     print(currentTimeString() + String.sColon + String.sSpace + message)
     #endif
+}
+
+//MARK: 类型判断和复制
+///判断是否是类对象
+func g_isObj(_ obj: Any) -> Bool
+{
+    type(of: obj) is AnyClass
+}
+
+///是否是NSObject对象
+func g_isObjc(_ obj: AnyObject) -> Bool
+{
+    type(of: obj) is NSObject.Type
+}
+
+///是否是NSObject子类
+func g_isOC(_ cls: AnyClass) -> Bool
+{
+    cls is NSObject.Type
 }
 
 ///返回一个拷贝的数据对象，如果是NSObject，那么返回copy对象；如果是Array/Dictionary，需要复制容器中的所有对象，返回新的容器和对象；其他返回原始值（基础类型、结构体、枚举等）
@@ -78,6 +98,7 @@ func g_objClassName(_ obj: AnyObject) -> String
     }
 }
 
+//MARK: 全局窗口和VC
 ///获取window对象
 func g_window() -> UIWindow
 {
@@ -88,70 +109,6 @@ func g_window() -> UIWindow
 func g_topVC() -> UIViewController
 {
     ControllerManager.shared.topVC
-}
-
-///判断字符串是否是有效字符串，无效字符串：nil、null、<null>、<nil>、""、"(null)"、NSNull
-func g_validString(_ str: String?) -> Bool
-{
-    DatasChecker.shared.checkValidString(str)
-}
-
-///判断是否有效对象，无效对象：nil，NSNull
-func g_validObject(_ obj: AnyObject?) -> Bool
-{
-    DatasChecker.shared.checkValidObject(obj)
-}
-
-///判断一个字符串是否都是数字
-func g_checkNum(_ str: String) -> Bool
-{
-    DatasChecker.shared.checkNumber(str)
-}
-
-///在线程中异步执行代码
-func g_async(onMain: Bool = true, action: @escaping VoidClosure)
-{
-    ThreadManager.shared.async(onMain: onMain, action: action)
-}
-
-///延时操作
-func g_after(_ interval: TimeInterval, onMain: Bool = true, action: @escaping VoidClosure)
-{
-    TimerManager.shared.after(interval: interval, onMain: onMain, action: action)
-}
-    
-///生成一个随机字符串
-func g_uuid() -> String
-{
-    EncryptManager.shared.uuidString()
-}
-
-///用于生成一类带时间和uuid的特定字符串，通常用来唯一标记一个key
-///形如："2022/02/02 11:11:11<=>skldffjslwfw9323rwjfxzskldf3fjww293jf<=>http://www.baidu.com/search=123"
-func g_identifier(_ id: String) -> String
-{
-    currentTimeString() + String.sSeprator + g_uuid() + String.sSeprator + id
-}
-    
-///获取设备id，在app安装周期内保持不变
-func g_deviceId() -> String
-{
-    ApplicationManager.shared.getDeviceId()
-}
-
-///des加密一个字符串
-func g_des(_ str: String, key: String) -> String
-{
-    EncryptManager.shared.des(str, desKey: key)
-}
-
-///des解密一个字符串
-func g_desDecrypt(_ str: String?, key: String) -> String?
-{
-    guard str != nil else {
-        return nil
-    }
-    return EncryptManager.shared.desDecrypt(str!, desKey: key)
 }
 
 ///全局截屏
@@ -172,6 +129,75 @@ func g_presentVC(_ vc: UIViewController, animated: Bool = true)
     ControllerManager.shared.presentViewController(vc, animated: animated)
 }
 
+//MARK: 字符串和对象有效性判断
+///判断字符串是否是有效字符串，无效字符串：nil、null、<null>、<nil>、""、"(null)"、NSNull
+func g_validString(_ str: String?) -> Bool
+{
+    DatasChecker.shared.checkValidString(str)
+}
+
+///判断是否有效对象，无效对象：nil，NSNull
+func g_validObject(_ obj: AnyObject?) -> Bool
+{
+    DatasChecker.shared.checkValidObject(obj)
+}
+
+///判断一个字符串是否都是数字
+func g_checkNum(_ str: String) -> Bool
+{
+    DatasChecker.shared.checkNumber(str)
+}
+
+//MARK: 异步操作
+///在线程中异步执行代码
+func g_async(onMain: Bool = true, action: @escaping VoidClosure)
+{
+    ThreadManager.shared.async(onMain: onMain, action: action)
+}
+
+///延时操作
+func g_after(_ interval: TimeInterval, onMain: Bool = true, action: @escaping VoidClosure)
+{
+    TimerManager.shared.after(interval: interval, onMain: onMain, action: action)
+}
+    
+//MARK: 随机标志符
+///生成一个随机字符串
+func g_uuid() -> String
+{
+    EncryptManager.shared.uuidString()
+}
+
+///用于生成一类带时间和uuid的特定字符串，通常用来唯一标记一个key
+///形如："2022/02/02 11:11:11<=>skldffjslwfw9323rwjfxzskldf3fjww293jf<=>http://www.baidu.com/search=123"
+func g_identifier(_ id: String) -> String
+{
+    currentTimeString() + String.sSeprator + g_uuid() + String.sSeprator + id
+}
+    
+///获取设备id，在app安装周期内保持不变
+func g_deviceId() -> String
+{
+    ApplicationManager.shared.getDeviceId()
+}
+
+//MARK: 加密和解密
+///des加密一个字符串
+func g_des(_ str: String, key: String) -> String
+{
+    EncryptManager.shared.des(str, desKey: key)
+}
+
+///des解密一个字符串
+func g_desDecrypt(_ str: String?, key: String) -> String?
+{
+    guard str != nil else {
+        return nil
+    }
+    return EncryptManager.shared.desDecrypt(str!, desKey: key)
+}
+
+//AMRK: 全局信息获取
 ///当前用户token
 var g_userToken: String? {
     UserManager.shared.currentUserToken
@@ -204,6 +230,7 @@ func g_price(_ fen: Int) -> NSAttributedString
                                         underlineColor: .clear)
 }
 
+//MARK: 全局提示，toast和alert，加载动画
 ///全局弹框
 func g_alert(title: String,
              message: String? = nil,
@@ -234,9 +261,10 @@ func g_toast(text: String, hideDelay: TimeInterval = 2.5, completion: ToastManag
 }
 
 ///全局加载提示
-func g_loading(mode: ToastManager.TMShowMode = .serial)
+func g_loading()
 {
-    ToastManager.shared.wantShowAnimate(mode: mode)
+    ToastManager.shared.hideHUD()
+    ToastManager.shared.wantShowAnimate()
 }
 
 ///全局停止加载提示
@@ -245,6 +273,7 @@ func g_endLoading()
     ToastManager.shared.hideHUD()
 }
 
+//MARK: 沙盒目录操作
 ///沙盒Documents目录，参数可追加子目录
 func g_documentsDir(subDir: String? = nil) -> NSString
 {
