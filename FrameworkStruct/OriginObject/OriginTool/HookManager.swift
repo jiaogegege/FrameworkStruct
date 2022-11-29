3e1b618b3fe200b4b1e528d02f4daba136ecc7bd
 jxj <jiangxuejiao@jinfeng.cn>
2022-11-29 17:25:13 +0800

    hookmanager

---
+++ FrameworkStruct/OriginObject/OriginTool/HookManager.swift
@@ -0,0 +1,17 @@
+//
+//  HookManager.swift
+//  FrameworkStruct
+//
+//  Created by  jggg on 2022/11/29.
+//
+
+/**
+ 用来代替散落在各处的生命周期方法
+ 封装一个闭包，并提供一些描述信息
+ 主要好处是可以统一注册和管理生命周期方法中执行的动作
+ */
+import UIKit
+
+class HookManager: NSObject {
+    
+}
